caa66ba1df9d48c31d9f90e7df7e1b9652b48191
grsendha <grsendha11@gmail.com>
2024-04-14 15:49:26 +0530

    UI design completed

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Search from "../components/Search";
+import SortRepo from "../components/SortRepo";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="m-4">
+      <Search />
+      <SortRepo />
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
+        <ProfileInfo />
+        <Repos />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
